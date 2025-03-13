b9317094511244f6d533c8ca8d00628ad3f55bba
Yasmim <ymdsc.coimbra85@gmail.com>
2025-03-12 21:14:06 -0300

    feat: adding functions to: validate empty input; get user data; show user data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,30 @@ import { screen } from "./objects/screen.js";
 
 export const searchInput = document.querySelector("#input-search");
 const searchButton = document.querySelector("#btn-search");
+
+function validateEmptyInput() {
+	if (searchInput.value === "") {
+		screen.renderNotWritten();
+		return true;
+	}
+}
+
+async function getUserData() {
+	const user = await getUser();
+
+	const repos = await getRepos();
+
+	userData.setInfo(user);
+	userData.setRepositories(repos);
+
+	return user;
+}
+
+async function showUserData() {
+	const user = await getUserData();
+
+	if (user.message === "Not Found") return screen.renderNotFound();
+
+	screen.renderUser(userData);
+	screen.renderRepositories(userData.repositories);
+}
