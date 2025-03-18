43e23c75e57362f22351c7c3f034de1c558b3967
Yasmim <ymdsc.coimbra85@gmail.com>
2025-03-17 22:35:19 -0300

    fix: filter and slice events to make sure the function will return 10 events already filtered by type

--- src/scripts/services/events.js
+++ src/scripts/services/events.js
@@ -2,15 +2,21 @@ import { baseUrl, defaultShownQuantity } from "../variables.js";
 import { searchInput } from "../index.js";
 
 async function getEvents() {
-    const userName = searchInput.value;
-    const url = `${baseUrl}${userName}/events?per_page=${defaultShownQuantity}`;
+	const userName = searchInput.value;
+	const url = `${baseUrl}${userName}/events`;
 
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (error) {
-        throw new Error("Eventos não encontrados!");
-    }
+	try {
+		const response = await fetch(url);
+		const events = await response.json();
+		const tenFilteredEvents = events
+			.filter(
+				(event) => event.type === "PushEvent" || event.type === "CreateEvent",
+			)
+			.slice(0, defaultShownQuantity);
+		return await tenFilteredEvents;
+	} catch (error) {
+		throw new Error("Eventos não encontrados!");
+	}
 }
 
 export { getEvents };
